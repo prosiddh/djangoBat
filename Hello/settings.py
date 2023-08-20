6ba557e4608da5ba676755f8df4bd34c66e0e2c8
prosiddh <133637249+prosiddh@users.noreply.github.com>
2023-08-20 16:46:02 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6$_-=svxwnolk_6bb%7y*k8=^70y91c)+n2ik0hjg67ct*u(k-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.159.4.9']
+ALLOWED_HOSTS = ['54.159.4.9:8000']
 
 
 # Application definition
